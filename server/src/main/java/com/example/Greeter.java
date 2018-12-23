8ba4882f8ddda4017ee36cf432358626a0fef080
Antonio Garcia <garcastillo00@gmail.com>
2018-12-23 18:52:47 +0100

    fdfdafkadkfldjaf dfj dkafjdk fjdasfjk dakfjfkd jkjfd

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
  /**
  * @author Bob
  * <b>Summary</b>
  */
-=======
-/**
- * This es para definir el paquete.
- */
 
->>>>>>> 2c8fde35d85fc94a2429622c91f50fdff9f152a0
 package com.example;
 
 /**
@@ -22,20 +16,13 @@ public class Greeter {
   public Greeter() {
 
   }
-<<<<<<< HEAD
- 
- /**
-   * @param someone; no se sabe que es
-   */
-  public final String greet(final String someone) {
-=======
 
   /**
  * @param esto es para alguien
  * @return el resultado
  */
 public final String greet(final String someone) {
->>>>>>> 2c8fde35d85fc94a2429622c91f50fdff9f152a0
+
     return String.format("Hello, %s!", someone);
   }
 }
